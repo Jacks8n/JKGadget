66007cd980bf628268c09b19976bc47ef1edcd4e
Jacks0n <cjdxz10@126.com>
2021-01-15 01:23:13 +0800

    rewrite parallelism

---
+++ IGI/inc/igiacceleration/parallel.h
@@ -0,0 +1,231 @@
+﻿#pragma once
+
+#include <atomic>
+#include <functional>
+#include <memory_resource>
+#include <thread>
+#include "igiacceleration/mem_arena.h"
+
+namespace igi {
+    template <typename TContext>
+    struct parallel_traits {
+        using context_ctor_t = std::function<TContext()>;
+
+        static inline const size_t DefaultTaskBufferSize = std::thread::hardware_concurrency() * 4;
+
+        static inline const size_t DefaultConsumerCount = std::max(std::thread::hardware_concurrency(), 2u) - 1;
+    };
+
+    template <typename TContext, typename... TJobArgs>
+    class parallel_job;
+
+    template <typename TContext>
+    class parallel_context : private parallel_traits<TContext> {
+        using typename parallel_traits<TContext>::context_ctor_t;
+        using parallel_traits<TContext>::DefaultTaskBufferSize;
+        using parallel_traits<TContext>::DefaultConsumerCount;
+
+        context_ctor_t _contextCtor;
+
+      public:
+        parallel_context() : parallel_context([]() { return TContext(); }) { }
+
+        template <typename TCtor>
+        parallel_context(TCtor &&contextCtor)
+            : _contextCtor(std::forward<TCtor>(contextCtor)) { }
+
+        template <typename... TArgs>
+        parallel_job<TContext, TArgs...> schedule(void (*func)(TContext &, TArgs...), size_t consumerCount = 0, size_t bufferSize = 0) {
+            if (!bufferSize)
+                bufferSize = DefaultTaskBufferSize;
+            if (!consumerCount)
+                consumerCount = DefaultConsumerCount;
+
+            return parallel_job<TContext, TArgs...>(_contextCtor, func, consumerCount, bufferSize);
+        }
+
+        template <typename TFn, typename = std::void_t<decltype(&std::remove_cvref_t<TFn>::template operator()<TContext>)>>
+        auto schedule(TFn &&fn, size_t consumerCount = 0, size_t bufferSize = 0) {
+            static_assert(std::is_empty_v<std::remove_cvref_t<TFn>>);
+
+            return schedule(Specialize(std::forward<TFn>(fn)), consumerCount, bufferSize);
+        }
+
+      private:
+        template <typename T>
+        struct spec_helper;
+
+        template <typename TClass, typename TContextArg, typename... TArgs>
+        struct spec_helper<void (TClass::*)(TContextArg, TArgs...) const> {
+            template <typename TFn>
+            static auto GetSpecialized() {
+                constexpr void (*ptr)(TContext &, TArgs && ...) = [](TContext &context, TArgs &&...args) {
+                    static_cast<TFn *>(nullptr)->operator()(context, args...);
+                };
+                return ptr;
+            }
+        };
+
+        template <typename TFn>
+        static auto Specialize(const TFn &) {
+            using func_t = std::remove_cvref_t<TFn>;
+            using fptr_t = decltype(&func_t::template operator()<TContext &>);
+
+            return spec_helper<fptr_t>::template GetSpecialized<func_t>();
+        }
+    };
+
+    template <typename TCtor>
+    parallel_context(TCtor &&) -> parallel_context<std::remove_cvref_t<std::invoke_result_t<TCtor>>>;
+
+    template <typename TContext, typename... TJobArgs>
+    class parallel_job : private parallel_traits<TContext> {
+        friend class parallel_context<TContext>;
+
+        using typename parallel_traits<TContext>::context_ctor_t;
+
+        enum class task_state : int { empty,
+                                      ready,
+                                      occupied };
+
+        using task_t       = std::tuple<TJobArgs...>;
+        using task_value_t = std::tuple<std::remove_cvref_t<TJobArgs>...>;
+        using task_ref_t   = std::tuple<std::add_lvalue_reference<TJobArgs>...>;
+        using job_t        = void (*)(TContext &, TJobArgs...);
+        using task_state_t = std::atomic<task_state>;
+
+        class consumer {
+            std::thread _thread;
+
+            parallel_job *_schedule;
+
+            TContext _context;
+
+            job_t _job;
+
+            size_t _cursor;
+
+            task_value_t _task;
+
+          public:
+            consumer(parallel_job *schedule, context_ctor_t &contextCtor, const job_t &job)
+                : _thread(consume, this), _schedule(schedule), _context(contextCtor()), _job(job), _cursor(0) {
+                _thread.detach();
+            }
+
+            consumer(const consumer &) = delete;
+            consumer(consumer &&)      = delete;
+
+            static void consume(consumer *consumer) {
+                parallel_job *const schedule = consumer->_schedule;
+
+                while (!schedule->_exitFlag) {
+                    size_t &cursor     = consumer->_cursor;
+                    task_value_t &task = consumer->_task;
+                    while (!schedule->tryRetrieveTask(cursor, &task))
+                        schedule->incrementConsumerCursor(&cursor);
+
+                    std::apply(consumer->_job, std::tuple_cat(std::forward_as_tuple(consumer->_context), task));
+                }
+
+                schedule->notifyExit();
+            }
+        };
+
+        std::shared_ptr<consumer[]> _consumers;
+
+        size_t _consumerCount;
+
+        std::shared_ptr<task_value_t[]> _tasks;
+
+        std::shared_ptr<task_state_t[]> _states;
+
+        size_t _bufferSize;
+
+        std::atomic<size_t> _producerCursor;
+
+        std::atomic<int> _exitFlag;
+
+        template <typename TJob>
+        parallel_job(context_ctor_t &contextCtor, TJob &&job, size_t consumerCount, size_t bufferSize)
+            : _consumers(context::AllocateSharedArray<consumer>(consumerCount)), _consumerCount(consumerCount),
+              _tasks(context::AllocateSharedArray<task_value_t>(bufferSize)),
+              _states(context::AllocateSharedArray<task_state_t>(bufferSize)),
+              _bufferSize(bufferSize), _producerCursor(0), _exitFlag(0) {
+            for (size_t i = 0; i < _consumerCount; i++)
+                context::Construct(&_consumers[i], this, contextCtor, std::forward<TJob>(job));
+        }
+
+      public:
+        parallel_job(const parallel_job &) = delete;
+        parallel_job(parallel_job &&)      = delete;
+
+        ~parallel_job() {
+            finish();
+            context::Destroy(_consumers.get(), _consumerCount);
+        }
+
+        template <typename... TArgs>
+        void issue(TArgs &&...args) {
+            task_state_t &state = _states[_producerCursor];
+
+            state.wait(task_state::ready);
+            state.wait(task_state::occupied);
+
+            new (&_tasks[_producerCursor]) task_t(std::forward<TArgs>(args)...);
+            state = task_state::ready;
+
+            incrementProducerCursor();
+        }
+
+        void finish() {
+            if (_exitFlag == -1)
+                return;
+
+            _exitFlag = _consumerCount;
+            for (int i = _consumerCount; i > 0; i--)
+                _exitFlag.wait(i);
+            _exitFlag = -1;
+        }
+
+      private:
+        size_t incrementCursor(size_t cursor, size_t n) {
+            igiassert(cursor + n < _bufferSize * 2);
+
+            cursor += n;
+            return cursor < _bufferSize ? cursor : cursor - _bufferSize;
+        }
+
+        template <typename T>
+        static void notifyProducer(std::atomic<T> &atomic) {
+            atomic.notify_one();
+        }
+
+        bool tryRetrieveTask(size_t index, task_value_t *task) {
+            task_state expected = task_state::ready;
+            task_state_t &state = _states[index];
+            if (state.compare_exchange_strong(expected, task_state::occupied)) {
+                new (task) task_value_t(std::move(_tasks[index]));
+                state = task_state::empty;
+                notifyProducer(state);
+                return true;
+            }
+            return false;
+        }
+
+        void incrementProducerCursor() {
+            _producerCursor = incrementCursor(_producerCursor, 1);
+            _producerCursor.notify_one();
+        }
+
+        void incrementConsumerCursor(size_t *cursor) {
+            *cursor = incrementCursor(*cursor, 1);
+            _producerCursor.wait(*cursor);
+        }
+
+        void notifyExit() {
+            _exitFlag.fetch_sub(1);
+            notifyProducer(_exitFlag);
+        }
+    };
+}  // namespace igi
--- IGI/inc/render.h
+++ IGI/inc/render.h
@@ -3,98 +3,72 @@
 #include <chrono>
 #include <memory_resource>
 #include <ostream>
-#include "igiacceleration/task_queue.h"
+#include "igiacceleration/parallel.h"
 #include "igicamera/camera.h"
 #include "igiintegrator/IIntegrator.h"
+#include "igimath/mcode.h"
 #include "igitexture/texture.h"
 
 namespace igi {
-    template <typename TCam>
-    inline void render(const scene &scene, const TCam &camera, const IIntegrator &integrator,
-                       texture_rgb &res, size_t spp = 1, std::ostream *log = nullptr) {
-        struct task {
-            size_t u, v;
-            color3 *res;
+    template <typename TCamera, typename TIntegrator>
+    void render(const scene &scene, TCamera &&camera, TIntegrator &&integrator,
+                texture_rgb &res, size_t spp = 1, std::ostream *log = nullptr) {
+        igiassert(spp > 0);
 
-            task()             = default;
-            task(const task &) = default;
-
-            task(size_t u, size_t v, color3 *res)
-                : u(u), v(v), res(res) {
-            }
-        };
-
-        struct context {
-            integrator_context ic;
-            unit_square_distribution usd;
-
-            context() = default;
-        };
-
-        res.clear(palette::black);
-
-        std::mutex m;
         const single w = res.getWidth(), h = res.getHeight();
-        auto group = worker_group<task, context>::DetachMax(
-            [&, w, h](std::pair<task, context> &pair) {
-                task &in(pair.first);
-                context &c(pair.second);
+        const single wInv = 1_sg / w, hInv = 1_sg / h, sppInv = 1_sg / spp;
+        parallel_context parallel([&]() {
+            return std::make_tuple(integrator_context(),
+                                   uniform_quad_distribution(vec2f::One(0_sg), vec2f(wInv, hInv)),
+                                   std::ref(camera), std::ref(integrator), std::ref(scene), spp, sppInv);
+        });
+        auto job = parallel.schedule([](auto &context, vec2f uv, color3 *res) {
+            auto &[ic, uqd, camera, integrator, scene, spp, sppInv] = context;
 
-                single p;
-                vec2f xy = c.usd(c.ic.pcg, &p);
-                xy[0] += static_cast<single>(in.u);
-                xy[1] += static_cast<single>(in.v);
+            ray ray;
+            single p;
+            vec2f sample;
 
-                ray r    = camera.getRay(xy[0] / w, xy[1] / h);
-                color3 i = integrator.integrate(scene, r, c.ic) * p;
-
-                std::scoped_lock sl(m);
-                *in.res = *in.res + i;
-            },
-            1024);
+            for (size_t i = 0; i < spp; i++) {
+                sample = uqd(ic.pcg, &p) + uv;
+                ray    = camera.getRay(sample);
+                *res += (integrator.integrate(scene, ray, ic) / p) * sppInv;
+            }
+        });
 
         const size_t total  = res.getHeight() * res.getWidth() * spp;
-        const single oneper = total / 100_sg, oneperInv = 100_sg / total;
-        size_t issued = 0_sg, totalIssued = 0_sg;
-        size_t prec;
-        if (log) {
-            prec = log->precision();
+        const size_t oneper = total / 100, modulo = total % 100;
+        size_t issued = 1, percent = 0, residue = 0;
 
-            log->precision(3);
-            *log << std::fixed;
-        }
+        static constexpr size_t BlockSize     = 16;
+        static constexpr size_t PixelPerBlock = BlockSize * BlockSize;
 
+        res.clear(palette::black);
         const auto start = std::chrono::high_resolution_clock::now();
-        for (size_t j = 0; j < res.getHeight(); j++)
-            for (size_t i = 0; i < res.getWidth(); i++)
-                for (size_t k = 0; k < spp; k++) {
-                    group->issue(i, j, &res.get(i, j));
+        auto issue       = [&](vec2u uv) {
+            job.issue(vec2f(uv), &res.at(uv[0], uv[1]));
 
-                    if (log && ++issued >= oneper) {
-                        const auto elapsed = std::chrono::high_resolution_clock::now() - start;
+            if (log && ++issued > oneper) {
+                const auto elapsed = std::chrono::high_resolution_clock::now() - start;
+                const auto ns      = std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed);
 
-                        totalIssued += issued;
-                        *log << oneperInv * totalIssued << "%\t"
-                             << elapsed.count() * 1e-9 << "s" << std::endl;
-                        issued = 0;
-                    }
-                }
-        group->waitFinish();
-
-        const single sppinv = 1_sg / spp;
-        for (size_t j = 0; j < res.getHeight(); j++)
-            for (size_t i = 0; i < res.getWidth(); i++) {
-                color3 &c = res.get(i, j);
-
-                c = c * sppinv;
+                *log << ++percent << "%\t" << ns.count() * 1e-9 << "s\n";
+                issued = (residue += modulo) > 100 ? (residue -= 100, 0) : 1;
             }
+        };
+        for (size_t v = 0, u = 0; v < h; u = 0) {
+            if (v + BlockSize < h)
+                for (; u + BlockSize < w; u += BlockSize) {
+                    mvec<2, unsigned> morton;
+                    for (size_t i = 0; i < PixelPerBlock; i++, ++morton)
+                        issue(vec2u(u, v) + morton.coord());
+                }
 
-        if (log) {
-            const auto elapsed = std::chrono::high_resolution_clock::now() - start;
-            *log << "finished in " << elapsed.count() * 1e-9 << "s" << std::endl;
-
-            log->precision(prec);
-            *log << std::defaultfloat;
+            const size_t bottom = v + BlockSize < h ? v + BlockSize : h;
+            for (; v < bottom; v++)
+                for (size_t i = u; i < w; i++)
+                    issue(vec2u(i, v));
         }
+        job.finish();
     }
 }  // namespace igi
